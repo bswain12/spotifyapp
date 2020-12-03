e97d219a48cd9f965f58184887670237d2a7ce06
sunwoopk <73982673+sunwoopk@users.noreply.github.com>
2020-12-03 18:09:26 -0500

    Auth2 attempt plz help me :(

---
+++ app/libs/android-auth-1.2.1/CHANGELOG.md
@@ -0,0 +1,22 @@
+Change Log
+==========
+
+## Version 1.2.0
+
+2019-12-05
+
+* Breaking changes: Rename classes from AuthenticationClassName to AuthorizationClassName
+* Pass state parameter in AuthorizationResponse
+
+2019-08-12
+
+* Add  method to clear Spotify and Facebook cookies to AuthenticationClient
+* Upgrade buildToolsVersion to 27.0.3
+* Replace deprecated compile keyword in gradle files
+
+## Version 1.1.0
+
+_2018-02-28_
+
+* Upgrade target and compile SDKs to 27
+* Upgrade android support libraries to 27.0.2
--- app/src/main/java/com/example/spotifyapp/MainActivity.java
+++ app/src/main/java/com/example/spotifyapp/MainActivity.java
@@ -8,26 +8,40 @@ import com.spotify.android.appremote.api.error.CouldNotFindSpotifyApp;
 import com.spotify.android.appremote.api.error.NotLoggedInException;
 import com.spotify.android.appremote.api.error.UserNotAuthorizedException;
 
-
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 import com.spotify.protocol.client.Subscription;
 import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
 
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
 public class MainActivity extends AppCompatActivity {
     private static final String CLIENT_ID = "2f184ad41615437489cfd03177eade83";
     private static final String REDIRECT_URI = "com.example.spotifyapp://callback/";
+    private static final int REQUEST_CODE = 1337;
     private SpotifyAppRemote mSpotifyAppRemote;
-
+    //this isnt a real redirect uri so it might mess up the token
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginInBrowser(this, request);
         Log.d("MainActivity","Connected. Mine yuh");
 
     }
@@ -36,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
         ConnectionParams connectionParams =
                 new ConnectionParams.Builder(CLIENT_ID)
                         .setRedirectUri(REDIRECT_URI)
@@ -70,6 +85,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void connected() {
+        // Find album cover using Picasso
+        ImageView album_artwork = (ImageView) findViewById(R.id.album_artwork);
+        Picasso.get().load("https://i.scdn.co/image/b2163e7456f3d618a0e2a4e32bc892d6b11ce673").into(album_artwork);
+
         // Play a playlist
         mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX2sUQwD7tbmL");
 
@@ -80,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
                     final Track track = playerState.track;
                     if (track != null) {
                         Log.d("MainActivity", track.name + " by " + track.artist.name);
+                        album_artwork.setImageResource(track.imageUri.hashCode());
                     }
                 });
     }
